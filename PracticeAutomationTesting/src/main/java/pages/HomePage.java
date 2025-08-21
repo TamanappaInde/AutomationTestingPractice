04570b3487431d0609305165494302d8ae4203a7
Tamanappa <itamanappa@gmail.com>
2025-08-21 12:15:04 +0530

    Added a new test case

--- PracticeAutomationTesting/src/main/java/pages/HomePage.java
+++ PracticeAutomationTesting/src/main/java/pages/HomePage.java
@@ -1,9 +1,12 @@
 package pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class HomePage {
 
@@ -24,15 +27,48 @@ public class HomePage {
 	// click on shop menu
 	@FindBy(xpath="//*[text()='Shop']")
 	private WebElement shopMenu;
-	@FindBy(xpath="//*[text()='Home']")
-	private WebElement homeMenu;
 	public void clickonShopmenu() {
 		shopMenu.click();
 	
 	}
+	// click on home menu
+	@FindBy(xpath="//*[text()='Home']")
+	private WebElement homeMenu;
 	public void clickonHomemenu() {
 		homeMenu.click();
 	}
 	
+	// verify three arrivals are present on home page
+	@FindBy(css=".product a img")
+	private List<WebElement> arrivals;
+	public void verifyarrivals() {
+		System.out.println("Arrivals Found: " + arrivals.size());
+		Assert.assertEquals(arrivals.size(), 3,"Expected exactly three arrivals on home page");
+	}
 	
+	// click on one arrival
+	@FindBy(xpath="//img[@alt='Selenium Ruby']")
+	private WebElement seleniumRuby;
+	public void clickOnArrival() {
+		seleniumRuby.click();
+	}
+	
+	// verify that the after clicks on arrival image product details page is open
+	@FindBy(xpath="//*[text()='Add to basket']")
+	private WebElement addtobasket;
+	public void verifyaddtobasketbutton() {
+		addtobasket.isDisplayed();
+	}
+	public void clickonaddtobasket() {
+		addtobasket.click();
+	}
+	
+	// verify book added to basket sucessfully
+	@FindBy(xpath="//*[text()=' “Selenium Ruby” has been added to your basket.']")
+	private WebElement booktext;
+	public void verifybookaddedsuccessfully() {
+		String actualText = "[VIEW BASKET “Selenium Ruby” has been added to your basket.]";
+		String expected = booktext.getText();
+		Assert.assertEquals(actualText, expected,"Wrong book selected");
+	}
 }
--- PracticeAutomationTesting/src/test/java/runners/TestRunner.java
+++ PracticeAutomationTesting/src/test/java/runners/TestRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions (
-		features = "src/test/resources/features",
-		glue = {"amazon.stepdefinitions","hooks"},
+		features = "C:\\Users\\Admin\\git\\PracticeAutomationTesting\\PracticeAutomationTesting\\src\\test\\resources\\features",
+		glue = {"stepdefinitions","hooks"},
 		plugin = {"pretty","html:target/cucumber-report.html","json:target/cucumber.json"},
 		tags = "@HomePage",
 		dryRun = false,
