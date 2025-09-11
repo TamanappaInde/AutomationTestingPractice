236c94fb56fc3cad39adb863c620a6d73eae019d
Tamanappa <itamanappa@gmail.com>
2025-09-11 17:38:41 +0530

    Added code for hot to drag and drop multiple elements

--- PracticeAutomationTesting/src/test/java/automationPractice/ElementDragAndDrop.java
+++ PracticeAutomationTesting/src/test/java/automationPractice/ElementDragAndDrop.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,10 +21,10 @@ public class ElementDragAndDrop {
 	
 	WebDriver driver;
 	
-	@Test
+	@Test(enabled=false)
 	public void verifyDragAndDrop() {
 		
-		driver = new ChromeDriver();
+		  driver = new ChromeDriver();
 		  driver.get("https://practice.automationtesting.in/");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -52,6 +53,50 @@ public class ElementDragAndDrop {
 		  Assert.assertNotEquals(initialLocation, newLocation);
 		  
 		  driver.close();
+	}
+	
+	@Test(enabled=true)
+	public void multipleDragDrop() {
+		
+		driver = new ChromeDriver();
+		  driver.get("https://practice.automationtesting.in/");
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		  
+		  driver.findElement(By.xpath("//*[text()='Demo Site']")).click();
+		  driver.findElement(By.xpath("//a[@href='Interactions.html']")).click();
+		  driver.findElement(By.xpath("//*[text()='Drag and Drop ']")).click();
+		  driver.findElement(By.xpath("//*[text()='Static ']")).click();
+		
+		  //locate all elements
+		  WebElement logo = driver.findElement(By.xpath("//img[@src='logo.png']"));
+		  WebElement toolimg = driver.findElement(By.xpath("//img[@src='original.png']"));
+		  WebElement seleniumlogo = driver.findElement(By.xpath("//img[@src='selenium.png']"));
+		  
+		  // locate dropable area
+		  WebElement droparea = driver.findElement(By.id("droparea"));
+		  
+		  // step 1 - select all images
+		  Actions actions = new Actions(driver);
+		  actions.keyDown(Keys.CONTROL)
+		         .click(logo)
+		         .click(toolimg)
+		         .click(seleniumlogo)
+		         .keyUp(Keys.CONTROL)
+		         .perform();
+		  // step 2 - select one drop
+		  WebElement[] logos = {logo,toolimg,seleniumlogo};
+		  
+		  for (WebElement img : logos) {
+			  actions.clickAndHold(img)
+		         .moveToElement(droparea)
+		         .release()
+		         .perform();
+		  }
+		  
+		  
+		  
+		  
 	}
 
 }
